d8a1ffb1b74054c1aa21d354a60dea97e894907d
Sheil Kumar <sk17@illinois.edu>
2021-05-21 13:22:32 -0500

    Implemented dict to move names to ticker

---
+++ stocks.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import yfinance as yf
+
+class AnalyzeStock:
+  def __init__(self):
+    Ticker_data = _getTickers
+    Name_data = _getNames 
+
+  def Analyze(self,ticker_list,corporation_list=None):
+    "Does analaysis of stocks based on list of names or ticker id's"
+    "Part 1: Take only ticker or corporation lists"
+    "Part 2: Efficiently run analysis for all tickers in list (Pandas and lambda fucntions)"
+    "Part 3: Return conclusions of analysis"
+    pass
+
+  def _getTickers(self):
+      
+    
+  
+  def _getNames(self):
+    pass 
